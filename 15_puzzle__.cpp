00eff3366c903c5afc0e16d2d7b71203c4145462
Aagosh Raj Srivastava <aagoshrajsrivastava10@gmail.com>
2024-11-30 23:03:28 +0530

    Update 15_puzzle__.cpp

--- 15_puzzle__.cpp
+++ 15_puzzle__.cpp
@@ -1,46 +1,109 @@
 #include <iostream>
-#define N 4
+#include <vector>
+#include <queue>
+#include <map>
+#include <algorithm>
+
 using namespace std;
 
-int getInvCount(int arr[]) {
-    int inv_count = 0;
-    for (int i = 0; i < N * N - 1; i++) {
-        for (int j = i + 1; j < N * N; j++) {
-            if (arr[j] && arr[i] && arr[i] > arr[j])
-                inv_count++;
+const int N = 4;
+const int goal[N][N] = {
+    {1, 2, 3, 4},
+    {5, 6, 7, 8},
+    {9, 10, 11, 12},
+    {13, 14, 15, 0}
+};
+
+struct Node {
+    vector<vector<int>> board;
+    int x, y, cost, level;
+    Node* parent;
+};
+
+int calculateCost(vector<vector<int>>& initial) {
+    int count = 0;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (initial[i][j] && initial[i][j] != goal[i][j]) {
+                count++;
+            }
         }
     }
-    return inv_count;
+    return count;
 }
 
-int findXPosition(int puzzle[N][N]) {
-    for (int i = N - 1; i >= 0; i--)
-        for (int j = N - 1; j >= 0; j--)
-            if (puzzle[i][j] == 0)
-                return N - i;
+bool isSafe(int x, int y) {
+    return (x >= 0 && x < N && y >= 0 && y < N);
 }
 
-bool isSolvable(int puzzle[N][N]) {
-    int invCount = getInvCount((int*)puzzle);
-    if (N & 1)
-        return !(invCount & 1);
-    else {
-        int pos = findXPosition(puzzle);
-        if (pos & 1)
-            return !(invCount & 1);
-        else
-            return invCount & 1;
+void printPath(Node* root) {
+    if (root == nullptr) {
+        return;
+    }
+    printPath(root->parent);
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cout << root->board[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+Node* newNode(vector<vector<int>>& board, int x, int y, int newX, int newY, int level, Node* parent) {
+    Node* node = new Node;
+    node->board = board;
+    swap(node->board[x][y], node->board[newX][newY]);
+    node->x = newX;
+    node->y = newY;
+    node->cost = calculateCost(node->board);
+    node->level = level;
+    node->parent = parent;
+    return node;
+}
+
+struct comp {
+    bool operator()(const Node* lhs, const Node* rhs) const {
+        return (lhs->cost + lhs->level) > (rhs->cost + rhs->level);
+    }
+};
+
+void solve(vector<vector<int>>& initial, int x, int y) {
+    priority_queue<Node*, vector<Node*>, comp> pq;
+    Node* root = newNode(initial, x, y, x, y, 0, nullptr);
+    pq.push(root);
+
+    while (!pq.empty()) {
+        Node* min = pq.top();
+        pq.pop();
+
+        if (min->cost == 0) {
+            printPath(min);
+            return;
+        }
+
+        int row[] = {1, 0, -1, 0};
+        int col[] = {0, -1, 0, 1};
+
+        for (int i = 0; i < 4; i++) {
+            if (isSafe(min->x + row[i], min->y + col[i])) {
+                Node* child = newNode(min->board, min->x, min->y, min->x + row[i], min->y + col[i], min->level + 1, min);
+                pq.push(child);
+            }
+        }
     }
 }
 
 int main() {
-    int puzzle[N][N] = {
-        {12, 1, 10, 2},
-        {7, 11, 4, 14},
-        {5, 0, 9, 15},
-        {8, 13, 6, 3},
+    vector<vector<int>> initial = {
+        {1, 2, 3, 4},
+        {5, 6, 0, 8},
+        {9, 10, 7, 12},
+        {13, 14, 11, 15}
     };
 
-    isSolvable(puzzle) ? cout << "Solvable" : cout << "Not Solvable";
+    int x = 1, y = 2; 
+    solve(initial, x, y);
+
     return 0;
 }
